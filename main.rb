fc084aad9686314c241e1246aa9e33f281121723
David Siaw <davidsiaw@gmail.com>
2016-04-19 14:55:00 +0900

    log inputs

--- main.rb
+++ main.rb
@@ -1,5 +1,24 @@
 require 'sinatra'
+require 'yaml'
 
-get '/' do
-  "Hello World!"
+get '*' do
+	open('get.log', 'a') do |f|
+		thing = {
+			params: params
+
+		}.to_yaml
+
+		f.puts thing
+	end
+end
+
+post '*' do
+	open('post.log', 'a') do |f|
+		thing = {
+			params: params
+
+		}.to_yaml
+
+		f.puts thing
+	end
 end

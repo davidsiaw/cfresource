46d81e369521fbab20865080ab0297af1614fb1e
David Siaw <davidsiaw@gmail.com>
2016-04-19 15:20:17 +0900

    environment

--- main.rb
+++ main.rb
@@ -1,24 +1,19 @@
 require 'sinatra'
 require 'yaml'
+require 'json'
 
 get '*' do
 	open('get.log', 'a') do |f|
-		thing = {
-			params: params
+		thing = JSON.parse(JSON.pretty_generate(request.env)).to_yaml
 
-		}.to_yaml
-
-		f.puts thing
+		puts thing
 	end
 end
 
 post '*' do
 	open('post.log', 'a') do |f|
-		thing = {
-			params: params
+		thing = JSON.parse(JSON.pretty_generate(request.env)).to_yaml
 
-		}.to_yaml
-
-		f.puts thing
+		puts thing
 	end
 end

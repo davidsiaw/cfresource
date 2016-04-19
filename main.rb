88d8490f3c8423705872d52ed8f8cd68ee84be0b
David Siaw <davidsiaw@gmail.com>
2016-04-19 15:42:28 +0900

    remote

--- main.rb
+++ main.rb
@@ -1,11 +1,9 @@
 require 'sinatra'
 require 'yaml'
 require 'json'
-require 'fileutils'
 
 get '*' do
-	FileUtils.mkdir_p "/var/log/apache2/"
-	open('/var/log/apache2/access.log', 'a') do |f|
+	open('get.log', 'a') do |f|
 		thing = JSON.parse(JSON.pretty_generate(request.env)).to_yaml
 
 		f.puts thing
@@ -13,8 +11,7 @@ get '*' do
 end
 
 post '*' do
-	FileUtils.mkdir_p "/var/log/apache2/"
-	open('/var/log/apache2/error.log', 'a') do |f|
+	open('post.log', 'a') do |f|
 		thing = JSON.parse(JSON.pretty_generate(request.env)).to_yaml
 
 		f.puts thing
